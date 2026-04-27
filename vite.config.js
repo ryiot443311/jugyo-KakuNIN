dd3a2eb04acf3476faf96f10a876c1b4ea56d61d
@_@ <ryo444333games@gmail.com>
2026-04-27 15:01:56 +0900

    Add Vite configuration for server and React plugin
    Configure Vite server settings for accessibility.

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true, // スマホなどの別端末からアクセス可能にする設定
+    port: 5173
+  }
+})
